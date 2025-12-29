a71285a751ae54a905b5b1c67c78749678f38cfb
kiu-chan <khanhk66uet@gmail.com>
2025-12-29 15:46:05 +0700

    refactor: Update Google Maps API key references in configuration

--- lib/utils/config_manager.dart
+++ lib/utils/config_manager.dart
@@ -2,11 +2,11 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ConfigManager {
   static String get googleMapsApiKeyAndroid {
-    return dotenv.env['GOOGLE_MAPS_API_KEY_ANDROID'] ?? '';
+    return dotenv.env['GOOGLE_MAPS_API_KEY'] ?? '';
   }
 
   static String get googleMapsApiKeyIOS {
-    return dotenv.env['GOOGLE_MAPS_API_KEY_IOS'] ?? '';
+    return dotenv.env['GOOGLE_MAPS_API_KEY'] ?? '';
   }
 
   static Future<void> initialize() async {
